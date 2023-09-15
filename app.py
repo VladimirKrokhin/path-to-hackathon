d0f78ba1345fe1cf303bf415ba8bae97fe71416a
Vladimir Krokhin <krohin2004@gmail.com>
2023-09-15 21:08:39 +0500

    Added render template

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask # импортируем класс Flask из библиотеки flask
+from flask import Flask, render_template # импортируем класс Flask из библиотеки flask
 
 app = Flask(__name__) # создаем класс приложения, __name__ - имя приложения
 
-@app.route("/")
+@app.route("/index") # декоратор
+@app.route("/") # еще один декоратор
 def index():
-    return "<h1>Index</h1>"
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__': # точка входа
-    app.run(debug=True) # запустить в режиме отладки
+    app.run(debug=True) # запустить приложение (в режиме отладки)
